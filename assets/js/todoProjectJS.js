dc8d59c3067b967b0278b2b6a9a6a9d5b0e15bff
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-03-01 19:46:43 -0500

    todoListProjectJS.js - added functionality to get the text from the imput and add it to the un-ordered list in the html using the jQuery append method.

--- assets/js/todoProjectJS.js
+++ assets/js/todoProjectJS.js
@@ -1,3 +1,19 @@
 $("ul").on("click", "li", function(event){
   $(this).toggleClass("selected");
 });
+
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  //13 is the key number for the "Enter/Return key"
+if(event.which === 13){
+  var inputEnteredText = $(this).val();
+  $(this).val(" ");
+  $("ul").append("<li><span>X </span>" + inputEnteredText + "</li>");
+}
+});
